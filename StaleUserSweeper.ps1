f1f2cf8e2ba94737b3ed20038f9a9e6ff6469c64
KaijuLogic <141070717+KaijuLogic@users.noreply.github.com>
2025-11-23 16:27:46 +1100

    Update StaleUserSweeper.ps1

--- StaleUserSweeper.ps1
+++ StaleUserSweeper.ps1
@@ -176,7 +176,7 @@ Function Find-InactiveUsers{
             #Filters out any accounts that were created within the max age limit, this makes sure
             #that any users that were recently created aren't disabled until their account reaches the threshold.
             Search-ADAccount @UserSearchParams |
-                Get-ADuser $StaleUser -properties $UserProps |
+                Get-ADuser -properties $UserProps |
                 Where-Object {$_.WhenCreated -lt (Get-Date).AddDays(-$UnusedDays)} |
                 Select-Object $UserProps
         }
